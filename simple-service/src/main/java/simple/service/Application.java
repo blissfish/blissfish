e2966be2e16fa8f39650f8a4be2ee345b8b6847f
Mark Evertz <mark.evertz@gmx.de>
2018-02-25 11:40:23 +0100

    Initial commit

---
+++ simple-service/src/main/java/simple/service/Application.java
@@ -0,0 +1,40 @@
+package simple.service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	@Value("${version}")
+	private String version;
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/")
+	public Message echo() {
+		return echo("Hello World!");
+	}
+
+	@RequestMapping("/{message}")
+	public Message echo(@PathVariable String message) {
+		Message data = new Message(counter.incrementAndGet(), message, version);
+		log.info(data.toString());
+		return data;
+	}
+
+}
