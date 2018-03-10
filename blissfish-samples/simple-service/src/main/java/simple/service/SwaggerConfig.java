069b7ba687d2ee114cae06861b8f61ac54b9554a
Mark Evertz <mark.evertz@gmx.de>
2018-03-10 15:48:05 +0100

    minor

--- blissfish-samples/simple-service/src/main/java/simple/service/SwaggerConfig.java
+++ blissfish-samples/simple-service/src/main/java/simple/service/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package simple.service;
 
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,12 +10,9 @@ import com.google.common.base.Predicate;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
@@ -30,8 +30,8 @@ public class SwaggerConfig {
 
 	private ApiInfo apiInfo() {
 		
-		return new ApiInfoBuilder().title("API")
-				.description("API reference")
+		return new ApiInfoBuilder().title("Blissfish API")
+				.description("Blissfish API reference")
 						.version("1.0").build();
 	}
 
